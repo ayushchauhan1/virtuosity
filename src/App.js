b2797cacee00fb6a31f78c71a516df590ec7d040
AkshitGuptaIITR <Gakshit92@gmail.com>
2021-05-05 15:13:18 +0530

    Footer

--- src/App.js
+++ src/App.js
@@ -6,23 +6,23 @@ import About from './components/aboutus';
 import Ourboard from './components/ourboard';
 import Services from './components/services';
 import Contacts from './components/contactus';
-import Footer from './components/footer';
+import Footer from './components/Footer';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Router>
-          <Header />
-          <Switch>
-            <Route exact={true} path="/" render={() => <Home />} />
-            <Route exact={true} path="/aboutus" render={() => <About />} />
-            <Route exact={true} path="/contactus" render={() => <Contacts />} />
-            <Route exact={true} path="/services" render={() => <Services />} />
-            <Route exact={true} path="/ourboard" render={() => <Ourboard />} />
-           
-          </Switch>
-          </Router>
+        <Header />
+        <Switch>
+          <Route exact={true} path="/" render={() => <Home />} />
+          <Route exact={true} path="/aboutus" render={() => <About />} />
+          <Route exact={true} path="/contactus" render={() => <Contacts />} />
+          <Route exact={true} path="/services" render={() => <Services />} />
+          <Route exact={true} path="/ourboard" render={() => <Ourboard />} />
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
