1309659fe3885187d0c86840407d5b6cffb3fb40
AkshitGuptaIITR <Gakshit92@gmail.com>
2021-05-05 19:56:42 +0530

    Footer Update.

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -11,7 +11,7 @@ const footer = () => {
             <div className="foot">
                 <div className="container">
                     <div className="row">
-                        <div className="col-md-5 text-left">
+                        <div className="col-md-5">
                             <div className="about">About Us- </div>
                             <div className="">
                                 <a href="" className="f-links">Our Board</a><br />
@@ -20,39 +20,39 @@ const footer = () => {
                         </div>
                         <div className="col-md-5 text-center">
                             <div className="row">
-                                <div className="col-md-1 text-left">
+                                <div className="col-1 text-center">
                                     <img src={phone} alt="" className="phone-img" />
                                 </div>
-                                <div className="col-md-5 text-left">
+                                <div className="col-5 text-left">
                                     <div className="num">+91-9999030920</div>
                                 </div>
                             </div>
-                            <div className="row">
-                                <div className="col-md-1 text-left">
+                            <div className="row text-center">
+                                <div className="col-1 text-center">
                                     <img src={telephone} alt="" className="phone-img tele" />
                                 </div>
-                                <div className="col-md-5 text-left">
+                                <div className="col-5 text-left">
                                     <div className="num tele-num">01203519356</div>
                                 </div>
                             </div>
                             <div className="row">
-                                <div className="col-md-1 text-left">
+                                <div className="col-1 text-center">
                                     <img src={mail} alt="" className="phone-img mail" />
                                 </div>
-                                <div className="col-md-5 text-left">
+                                <div className="col-5 text-left">
                                     <div className="num mail-id">info@virtuosityfinancial.com</div>
                                 </div>
                             </div>
                         </div>
                         <div className="col-md-2 ">
                             <div className="row connection">
-                                <div className="col-md-4 text-left">
+                                <div className="col-4 text-center">
                                     <a href=""><img src={facebook} alt="" className="social"/></a>
                                 </div>
-                                <div className="col-md-4 text-left">
+                                <div className="col-4 text-center">
                                     <a href=""><img src={linkedin} alt="" className="social"/></a>
                                 </div>
-                                <div className="col-md-4 text-left">
+                                <div className="col-4 text-center">
                                     <a href=""><img src={whatsapp} alt="" className="social"/></a>
                                 </div>
                             </div>
