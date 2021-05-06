cbaafbc3074fada9f1cad81ed970e003ec35e7bd
Dhwaj Gupta <57860123+dhwaj1902@users.noreply.github.com>
2021-05-06 19:36:55 +0530

    fds

--- src/components/contactus.js
+++ src/components/contactus.js
@@ -1,15 +1,27 @@
 import React, { Component } from "react";
 import axios from "axios";
 import { withSnackbar } from "react-simple-snackbar";
-import MapContainer from './map';
+import MapContainer from "./map";
+import MapGL, { Marker } from "react-map-gl";
+import Logo from "../assets/marker.png";
+const MAPBOX_TOKEN =
+  "pk.eyJ1IjoiZGh3YWpndXB0YSIsImEiOiJja29jeDEzbngzZWozMnFscG56Z3R1Yjd4In0.lN-Fv7XpL6p3noEvggH1QQ"; // Set your mapbox token here
+
 class Contacts extends Component {
   state = {
     name: "",
     contact: "",
     email: "",
     loading: false,
+    viewport: {
+      latitude: 28.570271,
+      longitude: 77.360486,
+      zoom: 14,
+      bearing: 0,
+      pitch: 0,
+    },
   };
- 
+
   handleSubmit = () => {
     this.setState({ loading: true });
     const { openSnackbar } = this.props;
@@ -17,12 +29,11 @@ class Contacts extends Component {
     arr.push(this.state.name);
     arr.push(this.state.contact);
     arr.push(this.state.email);
-  
+
     if (
       this.state.name === "" ||
       this.state.contact === "" ||
-      this.state.email === "" 
-      
+      this.state.email === ""
     ) {
       openSnackbar("Please fill all details");
       this.setState({ loading: false });
@@ -42,38 +53,63 @@ class Contacts extends Component {
     }
   };
   render() {
-    
     return (
-      <div >
-        <div className='row'>
-          <div className='col-lg-6 col-md-6 col-sm-12 '>
-            <div className='contactHeading '>Contact US</div>
-            <div className=' contactHeading2'>Fill  up  the  form  and  we’ll  contact  you  soon !</div>
-            <div className='iput'>
-            <input
-             type="text"
-             className="form-control "
-             onChange={(e) => this.setState({ name: e.target.value })}
-             placeholder="Enter Name"></input>
-             <input
-              type="text"
-              className="form-control "
-              onChange={(e) => this.setState({ contact: e.target.value })}
-              placeholder="Enter Contact Number"
-            ></input>
+      <div>
+        <div className="row">
+          <div className="col-lg-6 col-md-6 col-sm-12 ">
+            <div className="contactHeading ">Contact US</div>
+            <div className=" contactHeading2">
+              Fill up the form and we’ll contact you soon !
+            </div>
+            <div className="iput">
               <input
-              type="text"
-              className="form-control "
-              onChange={(e) => this.setState({ email: e.target.value })}
-              placeholder="Enter Email address"
-            ></input> 
-          
-             </div>
-             <button type="button" class="btn btn-dark  butt" onClick={this.handleSubmit}>Submit</button>
+                type="text"
+                className="form-control "
+                onChange={(e) => this.setState({ name: e.target.value })}
+                placeholder="Enter Name"
+              ></input>
+              <input
+                type="text"
+                className="form-control "
+                onChange={(e) => this.setState({ contact: e.target.value })}
+                placeholder="Enter Contact Number"
+              ></input>
+              <input
+                type="text"
+                className="form-control "
+                onChange={(e) => this.setState({ email: e.target.value })}
+                placeholder="Enter Email address"
+              ></input>
+            </div>
+            <button
+              type="button"
+              class="btn btn-dark  butt"
+              onClick={this.handleSubmit}
+            >
+              Submit
+            </button>
           </div>
-           <div className='col-lg-6 col-md-6 col-sm-12 block5'>
-           <MapContainer></MapContainer>
-           </div>
+          <div className="col-lg-6 col-md-6 col-sm-12 mt-5 pt-5 w-100">
+            {/* <MapContainer></MapContainer> */}
+            <MapGL
+              {...this.state.viewport}
+              height="70vh"
+              width="90%"
+              className="py-3"
+              mapStyle="mapbox://styles/mapbox/streets-v9"
+              onViewportChange={(viewport) => this.setState({ viewport })}
+              mapboxApiAccessToken={MAPBOX_TOKEN}
+            >
+              <Marker
+                latitude={28.570271}
+                longitude={77.360486}
+                offsetLeft={0}
+                offsetTop={0}
+              >
+                <img src={Logo} width="30%" />
+              </Marker>
+            </MapGL>
+          </div>
         </div>
       </div>
     );
